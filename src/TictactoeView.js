0c0f3c5e32b6ccfa6d15a5c59ab85cb16c19dd8e
Luiz Paulo <lppjunior@gmail.com>
2021-03-20 16:29:08 -0300

    Update package Run standard

--- src/TictactoeView.js
+++ src/TictactoeView.js
@@ -1,123 +1,122 @@
 /**
  * Privated class
  * Control DOM interface components
- * 
+ *
  * @this {TictactoeView}
  */
 class TictactoeView {
-	
-	/**
+  /**
 	 * @constructor
 	 * @param {config} Define all configurations
 	 **/
-	constructor(game) {
-		this.target = game.target;
-		game
-			.on("update", (e, data) => { this.update(data); })
-			.on("finish", (e, winner, position) => { 
-								this.finishGame(winner, position);
-						}
-					);
-	}
+  constructor (game) {
+    this.target = game.target
+    game
+      .on('update', (e, data) => { this.update(data) })
+      .on('finish', (e, winner, position) => {
+        this.finishGame(winner, position)
+      })
+  }
 
-	/**
+  /**
 	 * Render a HTML structure into target DOM object
-	 * 
+	 *
 	 **/
-	render() {
+  render () {
+    this.target.html(`
+		<table summary="grid">
+			<tbody>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>`)
 
-		this.target.html('\
-		<table summary="grid">\
-			<tbody>\
-				<tr>\
-					<td></td>\
-					<td></td>\
-					<td></td>\
-				</tr>\
-				<tr>\
-					<td></td>\
-					<td></td>\
-					<td></td>\
-				</tr>\
-				<tr>\
-					<td></td>\
-					<td></td>\
-					<td></td>\
-				</tr>\
-			</tbody>\
-		</table>');
+    this.initEvents()
+  }
 
-		this.initEvents();
-	}
-
-	/**
+  /**
 	 * Define events to column itens
-	 * 
+	 *
 	 **/
-	initEvents() {
-		let that = this;
-		this.itens = this.target.find("td");
-		this.itens.click((e) => { this.click(e); });
-	}
+  initEvents () {
+    this.itens = this.target.find('td')
+    this.itens.click((e) => { this.click(e) })
+  }
 
-	/**
+  /**
 	 * Execited on item click
-	 * 
+	 *
 	 * @param {Event} e Event from object clicked
-	 * 
+	 *
 	 **/
-	click(e) {
-		let target   = jQuery(e.target);
-		let position = this.itens.index(target);
-		jQuery(this.target).trigger("play", [position]);
-	}
+  click (e) {
+    const target = jQuery(e.target)
+    const position = this.itens.index(target)
+    jQuery(this.target).trigger('play', [position])
+  }
 
-	/**
+  /**
 	 * Executed when Trigger 'update' is called
 	 * Change style from checked item
-	 * 
+	 *
 	 * @param {array} board Current game board
-	 * 
+	 *
 	 **/
-	update(board) {
-		for(let pos in board) {
-			let style = "";
-			switch(board[pos]) {
-				case Tictactoe.CIRCLE: 
-					style = "circle";
-					break;
-				case Tictactoe.CROSS:
-					style = "cross"; 
-					break;
-			}
+  update (board) {
+    const { CIRCLE, CROSS } = Tictactoe
 
-			let td = this.itens.get(pos);
-			jQuery(td).attr("class", style);
-		}
-	}
+    for (const pos in board) {
+      let style = ''
+      switch (board[pos]) {
+        case CIRCLE:
+          style = 'circle'
+          break
+        case CROSS:
+          style = 'cross'
+          break
+      }
 
-	/**
+      const td = this.itens.get(pos)
+      jQuery(td).attr('class', style)
+    }
+  }
+
+  /**
 	 * Executed when Trigger 'finish' is called
 	 * Change style from checked item
-	 * 
+	 *
 	 * @param {array} board Current game board
-	 * 
+	 *
 	 **/
-	finishGame(winner, position) {
-		for(let pos in position) {
-			let td = this.itens.get(position[pos]);
-			jQuery(td).addClass("victory");
-		}
+  finishGame (winner, position) {
+    for (const pos in position) {
+      const td = this.itens.get(position[pos])
+      jQuery(td).addClass('victory')
+    }
 
-		let desc = "";
-		switch(winner) {
-			case Tictactoe.NONE: desc = "Deu velha"; break;
-			case Tictactoe.USER: desc = "Usuário ganhou"; break;
-			default: desc = "Computador ganhou"; break;
-		}
+    const { NONE, USER } = Tictactoe
 
-		let div = jQuery('<div class="winner opaque">' + desc + '</div>').click((e) => { this.target.parent(".box").remove(); });
-		this.target.parent().append(div);
+    let desc = ''
+    switch (winner) {
+      case NONE: desc = 'Deu velha'; break
+      case USER: desc = 'Usuário ganhou'; break
+      default: desc = 'Computador ganhou'; break
+    }
 
-	}
+    const div = jQuery('<div class="winner opaque">' + desc + '</div>').click((e) => { this.target.parent('.box').remove() })
+    this.target.parent().append(div)
+  }
 }
